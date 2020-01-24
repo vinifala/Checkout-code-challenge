6aded191f234ff799f856d063204d7e21b21a81c
Vini Falavigna <vini.df@gmail.com>
2020-01-24 15:46:41 -0300

    [FEAT] add Item

--- src/Item/Item.ts
+++ src/Item/Item.ts
@@ -1,1 +1,11 @@
-export class Item {}
+export class Item {
+  readonly sku: string
+  readonly name: string
+  readonly price: number
+
+  constructor({ sku, name, price }) {
+    this.sku = sku
+    this.name = name
+    this.price = price
+  }
+}
