b7e126168cedd1a4d22448f472aacda1da7fa029
Vini Falavigna <vini.df@gmail.com>
2020-01-24 23:07:07 -0300

    [TEST] add Bundle Discount tests

---
+++ src/Deals/BundleDiscount.test.ts
@@ -0,0 +1,59 @@
+import { BundleDiscount } from "./"
+import { Item } from "../Item"
+import { Catalog } from "../Catalog"
+import { Purchase } from "../Purchase"
+
+const ipd = new Item({
+  sku: "ipd",
+  name: "Super iPad",
+  price: 549.99,
+})
+const mbp = new Item({
+  sku: "mbp",
+  name: "MacBook Pro",
+  price: 1399.99,
+})
+const atv = new Item({
+  sku: "atv",
+  name: "Apple TV",
+  price: 109.5,
+})
+const vga = new Item({
+  sku: "vga",
+  name: "VGA adapter",
+  price: 30.0,
+})
+
+const catalog = new Catalog([mbp, ipd, vga, atv])
+
+describe("Gives an item for free when something else is purchased", () => {
+  const deal = new BundleDiscount({
+    catalog,
+    deal: {
+      requiredItems: {
+        sku: "mbp",
+        quantity: 1,
+      },
+      freeItem: {
+        sku: "vga",
+      },
+    },
+  })
+
+  it("should apply the discount", () => {
+    const purchase = new Purchase()
+      .add("mbp")
+      .add("atv")
+      .add("vga")
+
+    expect(deal.discount(purchase)).toBeCloseTo(30)
+  })
+  it("should not apply the discount", () => {
+    const purchase = new Purchase()
+      .add("mbp")
+      .add("atv")
+      .add("ipd")
+
+    expect(deal.discount(purchase)).toBe(0)
+  })
+})
--- src/Deals/index.ts
+++ src/Deals/index.ts
@@ -1,3 +1,4 @@
 export { Deal } from "./Deal"
 export { BulkDiscount } from "./BulkDiscount"
+export { BundleDiscount } from "./BundleDiscount"
 export { XForThePriceOfY } from "./XForThePriceOfY"
