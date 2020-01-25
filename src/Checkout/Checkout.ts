b48667fba2ff4d60181e60b32588c3b7e02ab327
Vini Falavigna <vini.df@gmail.com>
2020-01-25 11:21:26 -0300

    [FEAT] implement checkout

---
+++ src/Checkout/Checkout.ts
@@ -0,0 +1,77 @@
+import { Purchase } from "../Purchase"
+import {
+  BulkDiscount,
+  BulkDiscountInterface,
+  BundleDiscount,
+  BundleDiscountInterface,
+  Deal,
+  XForThePriceOfY,
+  XForThePriceOfYInterface,
+} from "../Deals"
+import { Catalog } from "../Catalog"
+
+export interface PricingRules {
+  items: {
+    sku: string
+    name: string
+    price: number
+  }[]
+  deals?: {
+    bulkDiscount: BulkDiscountInterface["deal"][]
+    bundleDiscount: BundleDiscountInterface["deal"][]
+    xForThePriceOfY: XForThePriceOfYInterface["deal"][]
+  }
+}
+
+export class Checkout {
+  private purchase: Purchase
+  private catalog: Catalog
+  private deals: Deal[]
+
+  constructor({ items, deals }: PricingRules) {
+    this.catalog = new Catalog(items)
+    this.deals = []
+    this.purchase = new Purchase()
+
+    if (deals) {
+      if (deals.bulkDiscount && deals.bulkDiscount.length > 0) {
+        deals.bulkDiscount.forEach(deal => {
+          this.deals.push(new BulkDiscount({ catalog: this.catalog, deal }))
+        })
+      }
+      if (deals.bundleDiscount && deals.bundleDiscount.length > 0) {
+        deals.bundleDiscount.forEach(deal => {
+          this.deals.push(new BundleDiscount({ catalog: this.catalog, deal }))
+        })
+      }
+      if (deals.xForThePriceOfY && deals.xForThePriceOfY.length > 0) {
+        deals.xForThePriceOfY.forEach(deal => {
+          this.deals.push(new XForThePriceOfY({ catalog: this.catalog, deal }))
+        })
+      }
+    }
+  }
+
+  scan(sku: string): void {
+    if (!this.catalog[sku]) {
+      throw "Invalid Item scanned"
+    }
+    this.purchase.add(sku)
+  }
+
+  total(): number {
+    const { items } = this.purchase
+
+    const subtotal = Object.keys(items).reduce(
+      (acc, key): number => acc + items[key] * this.catalog[key].price,
+      0
+    )
+
+    const discounts = this.deals.reduce(
+      (acc, deal) => acc + deal.discount(this.purchase),
+      0
+    )
+
+    return subtotal + discounts
+  }
+}
